6412b022bda65df92452d980ec3f0914e89e1218
Emilio Nicoli <emilio.nicoli@coxautoinc.com>
2019-01-01 23:41:18 -0700

    lambda fix

--- lambda.tf
+++ lambda.tf
@@ -26,7 +26,7 @@ resource "aws_lambda_function" "test_lambda" {
   filename         = "files.zip"
   function_name    = "index.js"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "exports.test"
+  handler          = "exports.handler"
   source_code_hash = "${base64sha256(file("files.zip"))}"
   runtime          = "nodejs8.10"
 
@@ -43,7 +43,7 @@ resource "aws_lambda_function" "test_lambda" {
 # and https://github.com/hashicorp/terraform/issues/8344
 data "archive_file" "init" {
   type        = "zip"
-  source_file = "files"
+  source_dir  = "files"
   output_path = "files.zip"
 }
 
