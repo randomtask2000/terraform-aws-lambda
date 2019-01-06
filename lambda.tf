7f7d228ed3573a6293b707fac179ce360aa82f66
Emilio Nicoli <emilio.nicoli@coxautoinc.com>
2019-01-05 23:05:37 -0700

    lambda fix

--- lambda.tf
+++ lambda.tf
@@ -28,8 +28,9 @@ EOF
 resource "aws_lambda_function" "test_lambda" {
   filename         = "files.zip"
   function_name    = "${var.lambda_name}"
+  description      = "Test lambda provisioned by Terraform"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "exports.handler"
+  handler          = "index.handler"
   source_code_hash = "${base64sha256(file("files.zip"))}"
   runtime          = "nodejs8.10"
 
